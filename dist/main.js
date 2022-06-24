482b136c11b1843b92dbf251879711bfdc053b7e
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-06-24 11:57:33 +1000

    initial commit

---
+++ dist/main.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const prompt_1 = __importDefault(require("prompt"));
+prompt_1.default.get(['username', 'email'], function (err, result) {
+    //
+    // Log the results.
+    //
+    console.log('Command-line input received:');
+    console.log('  username: ' + result.username);
+    console.log('  email: ' + result.email);
+});
---
+++ main.ts
@@ -0,0 +1,11 @@
+import prompt from 'prompt';
+import { Option } from 'fp-ts/Option';
+
+prompt.get(['username', 'email'], function (err, result) {
+  //
+  // Log the results.
+  //
+  console.log('Command-line input received:');
+  console.log('  username: ' + result.username);
+  console.log('  email: ' + result.email);
+});
